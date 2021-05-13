27ed53f3de2048a7aa0b7d303b0d511b43dc99ed
marcellodinizr <marcellodinizrocha@gmail.com>
2021-05-13 11:06:19 -0300

    general updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
+import Head from "next/head";
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
@@ -16,7 +19,8 @@ export default function Home() {
 			<section>
 				<div>
 					<Profile />
-					<CompletedChallenges/>
+					<CompletedChallenges />
+					<Countdown />
 				</div>
 				<div></div>
 			</section>
